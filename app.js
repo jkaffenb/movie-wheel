42a5b46b23c86129a202e3a9a3bc8cd9a6b8e521
jkaffenb <58486620+jkaffenb@users.noreply.github.com>
2022-03-27 18:01:42 -0400

    fixed text mostly

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ class MovieTable {
         let movie = '';
         if (this.size % 10 === 0) {
             //append a div
-            movie = document.createElement("div");
+            movie = document.createElement("li");
             const table = document.getElementById("table");
             table.appendChild(movie, table);
         }
 
-        movie = document.createElement("span");
+        movie = document.createElement("li");
         const movieName = document.createTextNode(movieTitle);
         movie.appendChild(movieName);
         movie.setAttribute("id", "movie" + this.size);
@@ -56,10 +56,10 @@ function drawWheel(num) {
     //hardcode in 1-4, if chunks are <90deg can use formula
     const circle = document.getElementById('circle');
     if (num === 1) {
-        const slice1 = document.createElement("div");
-        const slice2 = document.createElement("div");
-        const slice3 = document.createElement("div");
-        const slice4 = document.createElement("div");
+        const slice1 = document.createElement("li");
+        const slice2 = document.createElement("li");
+        const slice3 = document.createElement("li");
+        const slice4 = document.createElement("li");
         slice1.className = "circle-movie";
         slice2.className = "circle-movie";
         slice3.className = "circle-movie";
@@ -73,19 +73,25 @@ function drawWheel(num) {
         slice2.style.backgroundColor = color;
         slice3.style.backgroundColor = color;
         slice4.style.backgroundColor = color;
-        slice1.innerText = myTable.array[0];
-        circle.append(slice1)
-        circle.append(slice2)
-        circle.append(slice3)
-        circle.append(slice4)
+        //slice1.innerText = myTable.array[0];
+        const text = document.createElement("a");
+        text.innerText = myTable.array[0];
+        //text.style = "transform: skew(-45deg)";
+        text.style.display = "block";
+        text.style.paddingLeft = "5%";
+        slice1.appendChild(text);
+        circle.append(slice1);
+        circle.append(slice2);
+        circle.append(slice3);
+        circle.append(slice4);
         return;
     }
 
     if (num === 2) {
-        const slice1 = document.createElement("div");
-        const slice2 = document.createElement("div");
-        const slice3 = document.createElement("div");
-        const slice4 = document.createElement("div");
+        const slice1 = document.createElement("li");
+        const slice2 = document.createElement("li");
+        const slice3 = document.createElement("li");
+        const slice4 = document.createElement("li");
         slice1.className = "circle-movie";
         slice2.className = "circle-movie";
         slice3.className = "circle-movie";
@@ -100,8 +106,19 @@ function drawWheel(num) {
         color = '#' + Math.floor(Math.random() * 16777215).toString(16);
         slice3.style.backgroundColor = color;
         slice4.style.backgroundColor = color;
-        slice1.innerText = myTable.array[1];
-        slice3.innerText = myTable.array[0];
+        // slice1.innerText = myTable.array[1];
+        // slice3.innerText = myTable.array[0];
+        const text = document.createElement("a");
+        text.innerText = myTable.array[0];
+        text.style.display = "block";
+        text.style.paddingLeft = "5%";
+        slice1.appendChild(text);
+        const text1 = document.createElement("a");
+        text1.innerText = myTable.array[1];
+        text1.style.display = "block";
+        text1.style.paddingLeft = "5%";
+        slice1.appendChild(text);
+        slice3.appendChild(text1);
         circle.append(slice1);
         circle.append(slice2);
         circle.append(slice3);
@@ -109,18 +126,18 @@ function drawWheel(num) {
         return;
     }
     if (num === 3) {
-        const slice1 = document.createElement("div");
-        const slice2 = document.createElement("div");
-        const slice3 = document.createElement("div");
-        const slice4 = document.createElement("div");
-        const slice5 = document.createElement("div");
-        const slice6 = document.createElement("div");
-        const slice7 = document.createElement("div");
-        const slice8 = document.createElement("div");
-        const slice9 = document.createElement("div");
-        const slice10 = document.createElement("div");
-        const slice11 = document.createElement("div");
-        const slice12 = document.createElement("div");
+        const slice1 = document.createElement("li");
+        const slice2 = document.createElement("li");
+        const slice3 = document.createElement("li");
+        const slice4 = document.createElement("li");
+        const slice5 = document.createElement("li");
+        const slice6 = document.createElement("li");
+        const slice7 = document.createElement("li");
+        const slice8 = document.createElement("li");
+        const slice9 = document.createElement("li");
+        const slice10 = document.createElement("li");
+        const slice11 = document.createElement("li");
+        const slice12 = document.createElement("li");
         slice1.className = "circle-movie";
         slice2.className = "circle-movie";
         slice3.className = "circle-movie";
@@ -160,9 +177,27 @@ function drawWheel(num) {
         slice10.style.backgroundColor = color;
         slice11.style.backgroundColor = color;
         slice12.style.backgroundColor = color;
-        slice1.innerText = myTable.array[0];
-        slice5.innerText = myTable.array[1];
-        slice9.innerText = myTable.array[2];
+        // slice1.innerText = myTable.array[0];
+        // slice5.innerText = myTable.array[1];
+        // slice9.innerText = myTable.array[2];
+        const text = document.createElement("a");
+        text.innerText = myTable.array[0];
+        text.style = "transform: skew(-60deg)";
+        text.style.display = "block";
+        text.style.paddingLeft = "1%";
+        slice2.appendChild(text);
+        const text1 = document.createElement("a");
+        text1.innerText = myTable.array[1];
+        text1.style = "transform: skew(-60deg)";
+        text1.style.display = "block";
+        text1.style.paddingLeft = "1%";
+        slice6.appendChild(text1);
+        const text2 = document.createElement("a");
+        text2.innerText = myTable.array[2];
+        text2.style = "transform: skew(-60deg)";
+        text2.style.display = "block";
+        text2.style.paddingLeft = "1%";
+        slice10.appendChild(text2);
         circle.append(slice1);
         circle.append(slice2);
         circle.append(slice3);
@@ -178,10 +213,10 @@ function drawWheel(num) {
         return;
     }
     if (num === 4) {
-        const slice1 = document.createElement("div");
-        const slice2 = document.createElement("div");
-        const slice3 = document.createElement("div");
-        const slice4 = document.createElement("div");
+        const slice1 = document.createElement("li");
+        const slice2 = document.createElement("li");
+        const slice3 = document.createElement("li");
+        const slice4 = document.createElement("li");
         slice1.className = "circle-movie";
         slice2.className = "circle-movie";
         slice3.className = "circle-movie";
@@ -206,14 +241,21 @@ function drawWheel(num) {
 
     const deltaRotation = 360 / num;
     const skew = 90 - deltaRotation;
-    let slice = document.createElement("div");
-    let style = '';
+    let slice = document.createElement("li");
+    let text = document.createElement("a");
     for (let i = 0; i < num; i++) {
-        slice = document.createElement("div");
+        slice = document.createElement("li");
         slice.className = "circle-movie";
         slice.style = `transform: rotate(${i * deltaRotation}deg) skew(${skew}deg)`;
         slice.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-        slice.innerText = myTable.array[i];
+        // could scale text and padding, revist later
+        text = document.createElement("a");
+        text.innerText = myTable.array[i];
+        text.style = `transform: skew(-${skew}deg)`;
+        text.style.display = "block";
+        text.style.paddingLeft = "1%";
+
+        slice.appendChild(text);
         circle.append(slice);
     }
 }
